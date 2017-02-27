f09258328ffaeb00f0576b002651d010487f4a19
hanifmukhromin <hanifmukhromin@gmail.com>
2017-02-27 10:08:34 +0700

    Add files via upload

---
+++ New Project-20170227/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+
+     public static void main(String []args){
+        String nama = "hanif";
+        String kota = "ngabul city";
+        System.out.println("nama : " + nama + " kota : " + kota);
+     }
+}
