c6fd233360e33b730a200b7367accd7a5c9326aa
hanifmukhromin <hanifmukhromin@gmail.com>
2017-02-27 10:07:51 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class tugas4{
+
+     public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("angka pertama : ");
+        byte number = scanner.nextByte();
+        System.out.println("angka kedua : ");
+        byte number = scanner.nextByte();
+        int hasil = number1+number2;
+        System.out.println("jumlah " + " number1 + " + " number2 + " = " + hasil);
+     }
+}
