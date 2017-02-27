03020289868070c5939bc586f0371f21dd0b7b91
hanifmukhromin <hanifmukhromin@gmail.com>
2017-02-27 10:07:50 +0700

    Created by Coding Ground Client

---
+++ New Project-20170227/tugas2.java
@@ -0,0 +1,8 @@
+public class tugas2{
+    public static void main(String []args){
+        String nama = "hanif";
+        String kota = "ngabul city";
+        String prodi = "teknik informatika";
+        String fakultas = "saintek";
+        System.out.println("nama : " + nama + " kota : " + kota + " + prodi : " + prodi +" + fakultas : " + fakultas);
+} }
